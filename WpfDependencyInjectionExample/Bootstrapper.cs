25d1790dfd5b46097c59536f34c925119e0094d2
Tony Sedniov <megafinz@gmail.com>
2022-01-23 10:05:54 +0100

    Add an example of alternative DI container usage

---
+++ WpfDependencyInjectionExample/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace WpfDependencyInjectionExample;
+
+internal static class Bootstrapper
+{
+    // This is the so-called Composition Root. Here you register all dependencies and their lifetimes.
+    // When you call App.Current.Services.GetService<T>(), IServiceProvider (in this case this is Ninject kernel object)
+    // will try to instantiate T along with all it's dependencies (constructor parameters) recursively.
+    // E.g. when we call App.Current.Services.GetService<MainWindowViewModel>() inside MainWindow's constructor,
+    // it will create an instance of MainWindowViewModel, pass into it's constructor an instance of IFancyRepository
+    // (which concrete type is registered to be InMemoryFancyRepository, and since it's registered as a singleton,
+    // it will only be created once, and all subsequent attempts to instantiate it, like in FancyParser or SubMainViewModel,
+    // will yield the exact same instance).
+    public static IServiceProvider Bootstrap()
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<IFancyRepository, InMemoryFancyRepository>();
+        services.AddTransient<FancyParser>();
+        services.AddTransient<MainWindowViewModel>();
+        services.AddTransient<SubMainViewModel>();
+
+        return services.BuildServiceProvider();
+    }
+}
---
+++ WpfDependencyInjectionExample/NinjectBootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Ninject;
+using System;
+
+namespace WpfDependencyInjectionExample;
+
+internal static class NinjectBootstrapper
+{
+    // Just an alternative bootstrapper that show how to use a different DI container.
+    public static IServiceProvider Bootstrap()
+    {
+        var kernel = new StandardKernel();
+
+        kernel.Bind<IFancyRepository>().To<InMemoryFancyRepository>().InSingletonScope();
+        kernel.Bind<FancyParser>().ToSelf();
+        kernel.Bind<MainWindowViewModel>().ToSelf();
+        kernel.Bind<SubMainViewModel>().ToSelf();
+
+        return kernel;
+    }
+}
